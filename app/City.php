b035e6ec764ab46b9f89f8a34d0821e8642da102
Kemel <studentnote@mail.ru>
2020-07-04 22:25:44 +0400

    Добавил аутентификацию для юзера:

--- app/City.php
+++ app/City.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Class City
+ * @package App
+ * @property int $id
+ * @property string $title
+ */
 class City extends Model
 {
-    //
+    /**
+     * @return HasMany
+     */
+    public function events(): HasMany
+    {
+        return $this->hasMany(Event::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +15,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('auth', function (Request $request) {
+    $user = User::query()
+        ->select('api_token', 'password')
+        ->where('email',$request->get('email'))
+        ->first();
+    if (!is_null($user) && Hash::check($request->get('password'), $user->password)) {
+        return response()->json(['token' => $user->api_token]);
+    }
+    return response()->json(['error' => 'Ошибка авторизации'], 422);
 });
+
 Route::namespace('Api')->middleware('auth:api')->group(function (){
     Route::apiResource('members','MemberController');
 });
